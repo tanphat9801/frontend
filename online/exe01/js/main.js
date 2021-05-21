752430fa15a0d98983a9dab5e1a24758e7ab6b98
LAPTOP-I2B0SOER\TAN PHAT <tanphat9801@gmail.com>
2021-05-21 19:54:23 +0700

    baitaponl

---
+++ online/exe01/js/main.js
@@ -0,0 +1,30 @@
+    var a = document.getElementsByClassName('box');
+    var c = [];
+    
+    for (var i = 0; i < a.length; i++) {
+       var b = a[i].clientHeight;
+        c.push(b);
+    }
+    
+    var maxHeight = Math.max(...c);
+    console.log (maxHeight);
+    
+    for (var i = 0; i < a.length; i++) {
+
+        a[i].style.background = 'red';
+
+        if ( (i+1) % 2 == 0){
+            a[i].style.background = 'yellow';
+        }
+    }
+
+ 
+    
+  
+    
+
+    
+
+
+
+    
